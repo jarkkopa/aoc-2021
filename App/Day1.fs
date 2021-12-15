501e178e795d195f6ad0912b506e5bad8bf8d051
Jarkko Palokangas <hpmandito@gmail.com>
2021-12-15 18:22:38 +0200

    Add project files with day 1 solution

---
+++ App/Day1.fs
@@ -0,0 +1,51 @@
+module Day1
+
+open System.IO
+
+let readInput () =
+    File.ReadAllLines "Resources/day1-input.txt"
+    |> Array.map int
+    |> Array.toList
+
+let depthTrend x =
+    match x with
+    | (a, b) when a < b -> "increased"
+    | (a, b) when a > b -> "decreased"
+    | (a, b) when a = b -> "no change"
+    | _ -> "N/A"
+
+let rec solve list r =
+    match list with
+    | [_] -> r
+    | head::tail ->
+        let next = (head, List.head tail) |> depthTrend
+        let newR = r @ [next]
+        solve tail newR
+    | _ -> r
+
+let howManyIncreases input =
+    let filterIncreased = List.filter (fun x -> x = "increased")
+
+    (input, []) 
+    ||> solve 
+    |> filterIncreased
+    |> List.length
+
+let rec sumRollingThree list r =
+    match list with
+    | [ _ ; _] -> r
+    | head::tail -> 
+        let newR = r @ [head :: List.take 2 tail |> List.sum]
+        sumRollingThree tail newR
+    | _ -> r
+
+let day1part1 () =
+    readInput()
+    |> howManyIncreases
+
+let day1part2 () =
+    let input = readInput()
+
+    (input, [])
+    ||> sumRollingThree 
+    |> howManyIncreases
---
+++ App/Program.fs
@@ -0,0 +1,7 @@
+open Day1
+
+[<EntryPoint>]
+let main argv =
+    day1part1() |> printfn "Day 1-1 result: %i"
+    day1part2() |> printfn "Day 1-2 result: %i"
+    0

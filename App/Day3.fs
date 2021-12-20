acbdec373f3deb7c1f0bcee7a321a55f27e8ac63
Jarkko Palokangas <hpmandito@gmail.com>
2021-12-20 20:49:31 +0200

    Add work in progress solution for day 3 part 2

--- App/Day3.fs
+++ App/Day3.fs
@@ -7,6 +7,23 @@ let readInput =
     File.ReadAllLines "Resources/day3-input.txt"
     |> Array.toSeq
 
+let readInputTest =
+    [|
+        "00100";
+        "11110";
+        "10110";
+        "10111";
+        "10101";
+        "01111";
+        "00111";
+        "11100";
+        "10000";
+        "11001";
+        "00010";
+        "01010";
+    |]
+    |> Array.toSeq
+
 let toBit t =
     let higher x y =
         if snd x > snd y then fst x
@@ -29,3 +46,53 @@ let day3part1 =
     let mask = Seq.replicate bits.Length "1" |> Seq.reduce(+) |> toInt
     let alpha = gamma ^^^ mask
     gamma * alpha
+
+let bitEquals idx expected (data:string) =
+    data.[idx] = expected
+
+let higher def x y =
+    if snd x > snd y then fst x
+    elif snd x < snd y then fst y
+    else def
+
+let lower def x y =
+    if snd x < snd y then fst x
+    elif snd x > snd y then fst y
+    else def
+
+let bitCounts idx bits =
+    bits
+    |> Seq.transpose
+    |> Seq.map Seq.toList
+    |> Seq.item idx
+    |> List.countBy id
+
+let day3part2 =
+    let input = readInput
+
+    let filterCommon compare def idx bits= 
+        let common = bitCounts idx bits
+        let c = (common.[0], common.[1])
+            ||> compare def
+        bits |> List.filter (fun (x:string) -> x.[idx] = c)
+
+    let rec findValue compare def i values =
+        match values with
+        | [_] -> values
+        | x -> (filterCommon compare def i x) |> findValue compare def (i+1)
+    
+    let oxygen =
+        input
+        |> Seq.toList
+        |> findValue higher '1' 0
+        |> Seq.head
+        |> toInt
+
+    let co2 =
+        input
+        |> Seq.toList
+        |> findValue lower '0' 0
+        |> Seq.head
+        |> toInt
+    
+    oxygen*co2
--- App/Program.fs
+++ App/Program.fs
@@ -4,9 +4,10 @@ open Day3
 
 [<EntryPoint>]
 let main argv =
-    day1part1 |> printfn "Day 1-1 result: %i"
-    day1part2 |> printfn "Day 1-2 result: %i"
-    day2 withPos |> printfn "Day 2-1 result: %i"
-    day2 withAim |> printfn "Day 2-2 result: %i"
-    day3part1 |> printfn "Day 3-1 result: %i"
+    // day1part1 |> printfn "Day 1-1 result: %i"
+    // day1part2 |> printfn "Day 1-2 result: %i"
+    // day2 withPos |> printfn "Day 2-1 result: %i"
+    // day2 withAim |> printfn "Day 2-2 result: %i"
+    // day3part1 |> printfn "Day 3-1 result: %i"
+    day3part2 |> printfn "Day 3-2 result: %A"
     0

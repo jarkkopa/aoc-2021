7064e11008a0e60470be0c5e068f0f0a506a6c78
Jarkko Palokangas <hpmandito@gmail.com>
2021-12-22 23:27:11 +0200

    Refacto day 3 part 2

--- App/Day3.fs
+++ App/Day3.fs
@@ -5,31 +5,14 @@ open System.IO
 
 let readInput =
     File.ReadAllLines "Resources/day3-input.txt"
-    |> Array.toSeq
-
-let readInputTest =
-    [|
-        "00100";
-        "11110";
-        "10110";
-        "10111";
-        "10101";
-        "01111";
-        "00111";
-        "11100";
-        "10000";
-        "11001";
-        "00010";
-        "01010";
-    |]
-    |> Array.toSeq
+    |> Array.toList
 
 let toBit t =
     let higher x y =
         if snd x > snd y then fst x
         else fst y
         
-    t |> List.pairwise |> List.map (fun x -> x ||> higher |> string)
+    t |> Seq.pairwise |> Seq.map (fun x -> x ||> higher |> string)
 
 let toInt x = Convert.ToInt32(x, 2)
 
@@ -37,8 +20,7 @@ let day3part1 =
     let bits =
         readInput
         |> Seq.transpose
-        |> Seq.map Seq.toList
-        |> Seq.map (List.countBy id)
+        |> Seq.map (Seq.countBy id)
         |> Seq.map toBit
         |> Seq.concat
         |> Seq.reduce(+)
@@ -47,52 +29,42 @@ let day3part1 =
     let alpha = gamma ^^^ mask
     gamma * alpha
 
-let bitEquals idx expected (data:string) =
-    data.[idx] = expected
-
-let higher def x y =
-    if snd x > snd y then fst x
-    elif snd x < snd y then fst y
-    else def
-
-let lower def x y =
-    if snd x < snd y then fst x
-    elif snd x > snd y then fst y
-    else def
-
-let bitCounts idx bits =
-    bits
-    |> Seq.transpose
-    |> Seq.map Seq.toList
-    |> Seq.item idx
-    |> List.countBy id
-
 let day3part2 =
-    let input = readInput
+    let significant def (items:(char*int)list) =
+        if snd items.[0] = snd items.[1] then def
+        else fst items.[0]
 
-    let filterCommon compare def idx bits= 
-        let common = bitCounts idx bits
-        let c = (common.[0], common.[1])
-            ||> compare def
-        bits |> List.filter (fun (x:string) -> x.[idx] = c)
+    let bitCounts sorter idx (bits: string list) =
+        bits
+        |> Seq.transpose
+        |> Seq.item idx
+        |> Seq.toList
+        |> List.countBy id
+        |> sorter snd
 
-    let rec findValue compare def i values =
+    let rec findValue sorter def i values =
         match values with
         | [_] -> values
-        | x -> (filterCommon compare def i x) |> findValue compare def (i+1)
+        | x ->
+            let wantedBit = 
+                bitCounts sorter i values
+                |> significant def
+
+            values
+            |> List.filter (fun x -> x.[i] = wantedBit)
+            |> findValue sorter def (i + 1)
     
+    let input = readInput
     let oxygen =
         input
-        |> Seq.toList
-        |> findValue higher '1' 0
+        |> findValue List.sortByDescending '1' 0
         |> Seq.head
         |> toInt
 
     let co2 =
         input
-        |> Seq.toList
-        |> findValue lower '0' 0
+        |> findValue List.sortBy '0' 0
         |> Seq.head
         |> toInt
-    
-    oxygen*co2
+
+    oxygen * co2
